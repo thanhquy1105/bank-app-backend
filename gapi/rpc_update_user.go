b3cef428ec4e83f172fa0fd3ddb74f3018627b10
Thành Quý <49222110+thanhquy1105@users.noreply.github.com>
2023-09-30 16:42:01 +0700

    Build gRPC update API with optional parameters

---
+++ gapi/rpc_update_user.go
@@ -0,0 +1,67 @@
+package gapi
+
+import (
+	"context"
+
+	"github.com/lib/pq"
+	db "github.com/thanhquy1105/simplebank/db/sqlc"
+	"github.com/thanhquy1105/simplebank/pb"
+	"github.com/thanhquy1105/simplebank/util"
+	"github.com/thanhquy1105/simplebank/val"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest) (*pb.CreateUserResponse, error) {
+	violations := validateCreateUserRequest(req)
+	if violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
+	hashedPassword, err := util.HashPassword(req.GetPassword())
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to hash password: %s", err)
+	}
+
+	arg := db.CreateUserParams{
+		Username:       req.GetUsername(),
+		HashedPassword: hashedPassword,
+		FullName:       req.GetFullName(),
+		Email:          req.GetEmail(),
+	}
+
+	user, err := server.store.CreateUser(ctx, arg)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "unique_violation":
+				return nil, status.Errorf(codes.AlreadyExists, "username already exists: %s", err)
+
+			}
+		}
+		return nil, status.Errorf(codes.Internal, "failed to create user: %s", err)
+
+	}
+
+	rsp := &pb.CreateUserResponse{
+		User: convertUser(user),
+	}
+	return rsp, nil
+
+}
+
+func validateCreateUserRequest(req *pb.CreateUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := val.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, fieldViolation("username", err))
+	}
+	if err := val.ValidatePassword(req.GetPassword()); err != nil {
+		violations = append(violations, fieldViolation("password", err))
+	}
+	if err := val.ValidateFullName(req.GetFullName()); err != nil {
+		violations = append(violations, fieldViolation("full_name", err))
+	}
+	if err := val.ValidateEmail(req.GetEmail()); err != nil {
+		violations = append(violations, fieldViolation("email", err))
+	}
+	return violations
+}
